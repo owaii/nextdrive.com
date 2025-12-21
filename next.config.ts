e03f5ff40ebe4cb1bbe0d7b13e0a2c1c4aa163b6
owaii <tehnikumwiktor@gmail.com>
2025-12-21 22:59:47 +0100

    Update basePath in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  basePath: "/next-drive.com",
 };
 
 export default nextConfig;
